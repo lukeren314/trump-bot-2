4e81173a90855761e7c7a91a5176c0c3ef210c98
lukeren314 <lukeren314@gmail.com>
2019-07-29 18:12:46 -0700

    added an output log

--- train_bot.py
+++ train_bot.py
@@ -1,4 +1,5 @@
 import subprocess
 
 print('training model')
-subprocess.call(["th train.lua -gpuid -1 -data_dir data/trumptweets -rnn_size 200 -num_layers 2 -dropout 0.5"])
+subprocess.call(
+    ["th train.lua -gpuid -1 -data_dir data/trumptweets -rnn_size 300 -num_layers 3 -dropout 0.5"])
